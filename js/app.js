9261789ed7c4ab1784ed9dd569b03e6d1223bd1f
Matt Kistler <Matt.A.Kistler@gmail.com>
2016-06-07 19:45:22 -0500

    added google and got iframe working

--- js/app.js
+++ js/app.js
@@ -26,15 +26,17 @@ $("#reddit").on("click", function(){
 			}
 
 			articles.forEach(function(article) {
-				var articleHtml = processHandlebarsTemplate('reddit-article', articles) //look this up
+				var articleHtml = processHandlebarsTemplate('reddit-article', article) //look this up
 				var $articleContainer = $(articleHtml).appendTo('#main')
 
 				$articleContainer.on('click', 'a.title', function(event) {
-					console.log('You clicked on the following article:')
-					console.log(article)
+					$("#popUp").removeClass("hidden")
+					$("#fullart").attr("src", article.URL)
+					$(".popUpAction").attr("href", article.URL)
+					$("#popUp").removeClass("loader")
 				})
 			})
-			
+
 			$("#loadingmessage").attr("style","display: none")
 		},
 		error: function() {
@@ -43,34 +45,92 @@ $("#reddit").on("click", function(){
 	})
 })
 
-// $("#digg").on("click", function(){
-// 	$("#main").empty()
-// 	$("#loadingmessage").removeAttr("style")
-// 	$.ajax({
-// 		type: 'GET',
-// 		url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
-// 		success: function(response) {
-// 			var templateSource = $('#digg-article').html();
-// 			var compiledTemplate = Handlebars.compile(templateSource);
-// 			var articles = []
+$("#digg").on("click", function(){
+	$('#source').text("Digg")
+	$("#main").empty()
+	$("#loadingmessage").removeAttr("style")
+	$.ajax({
+		type: 'GET',
+		url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
+		success: function(response) {
+			var templateSource = $('#digg-article').html();
+			var compiledTemplate = Handlebars.compile(templateSource);
+			var articles = []
 
-// 			for (var i = 0; i < response.data.feed.length; i = i + 1) {
-// 				articles.push({
-// 					Title: response.data.feed[i].content.description,
-// 					Score: response.data.feed[i].digg_score,
-// 					Tag: response.data.feed[i].content.tags[0].display,
-// 					Thumb: response.data.feed[i].content.media.images[0].url,
-// 					URL: response.data.feed[i].content.original_url
-// 				})
-// 			}
+			for (var i = 0; i < response.data.feed.length; i = i + 1) {
+				articles.push({
+					Title: response.data.feed[i].content.description,
+					Score: response.data.feed[i].digg_score,
+					Tag: response.data.feed[i].content.tags[0].display,
+					Thumb: response.data.feed[i].content.media.images[0].url,
+					URL: response.data.feed[i].content.original_url
+				})
+			}
 
-// 			var generatedHtml = compiledTemplate(articles);
-// 			$('#main').append(generatedHtml);
-// 			$('#source').text("Digg")
-// 			$("#loadingmessage").attr("style","display: none")
-// 		},
-// 		error: function() {
-// 			console.log('Your request failed! Why!!?!?!?!')
-// 		}
-// 	})
-// })
+			articles.forEach(function(article) {
+				var articleHtml = processHandlebarsTemplate('digg-article', article) //look this up
+				var $articleContainer = $(articleHtml).appendTo('#main')
+
+				$articleContainer.on('click', 'a.title', function(event) {
+					$("#popUp").removeClass("hidden")
+					$("#fullart").attr("src", article.URL) //find a graceful way to handle this
+					// $("#fullart").attr("src", article.URL).error(function(){
+					// 	$("#fullart").addClass("hidden")
+					// 	$("#fullart").text("Iframes are not supported")
+					// })
+					$(".popUpAction").attr("href", article.URL)
+					$("#popUp").removeClass("loader") //find a way to make this appear until article loads
+				})
+			})
+
+			$("#loadingmessage").attr("style","display: none")
+		},
+		error: function() {
+			console.log('Your request failed! Why!!?!?!?!')
+		}
+	})
+})
+
+$("#google").on("click", function(){
+	$('#source').text("Google")
+	$("#main").empty()
+	$("#loadingmessage").removeAttr("style")
+	$.ajax({
+		type: 'GET',
+		url: 'https://accesscontrolalloworiginall.herokuapp.com/http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http%3A%2F%2Fnews.google.com%2Fnews%3Foutput%3Drss',
+		success: function(response) {
+			var articles = []
+
+			for (var i = 0; i < response.responseData.feed.entries.length; i = i + 1) {
+				articles.push({
+					Title: response.responseData.feed.entries[i].title,
+					Tag: response.responseData.feed.entries[i].categories[0],
+					URL: response.responseData.feed.entries[i].link
+				})
+			}
+
+			articles.forEach(function(article) {
+				var articleHtml = processHandlebarsTemplate('google-article', article) //look this up
+				var $articleContainer = $(articleHtml).appendTo('#main')
+
+				$articleContainer.on('click', 'a.title', function(event) {
+					$("#popUp").removeClass("hidden")
+					$("#fullart").attr("src", article.URL)
+					$(".popUpAction").attr("href", article.URL)
+					$("#popUp").removeClass("loader")
+				})
+			})
+
+			$("#loadingmessage").attr("style","display: none")
+		},
+		error: function() {
+			alert('Your request failed! Why!!?!?!?!')
+		}
+	})
+})
+
+$(".closePopUp").on("click", function() {
+	$("#popUp").addClass("hidden")
+	$("#fullart").attr("src", "")
+	$(".popUpAction").attr("href", "")
+})
